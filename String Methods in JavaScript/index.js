14e50e17310b3f3ed4e4e386a091f2370429b5d7
bega <behinmaksym@gmail.com>
2024-11-02 15:08:31 +0300

    Add files via upload

---
+++ String Methods in JavaScript/index.js
@@ -0,0 +1,106 @@
+//                       String methods = allow you to manipulate and work with text (strings)
+
+//                       indexOf methods
+//let userName = "Name"
+
+//userName.indexOf("a");
+
+//console.log(userName.indexOf("a")); // (0, 1, 2, 3, 4)
+
+
+
+//                       Trim methods
+//let userName = "Name";
+
+//userName = userName.trim();
+
+//console.log(userName);
+
+//userName.charAt(0)
+//console.log(userName.charAt(0));
+
+
+//                         toUpperCase methods
+//let userName = "Name"
+
+//userName = userName.toUpperCase("");
+
+//console.log(userName);
+
+//                          toLowerCase methods
+
+//let userName = "Name";
+
+//userName = userName.repeat(3);
+
+//console.log(userName);
+
+//                         .starWith() methods
+/* 
+let userName = "  Name";
+
+let result = userName.startsWith("   ");
+
+console.log(result);
+
+if (result) {
+    console.log("Your usename cant begin with ' ' ");
+}
+else {
+    console.log(userName);
+}
+*/
+
+
+/* 
+let userName = "  Name";
+
+let result = userName.endsWith("   ");
+
+console.log(result);
+
+if (result) {
+    console.log("Your usename cant end begin with ' ' ");
+}
+else {
+    console.log(userName);
+}
+*/
+
+//                                includes methods
+
+/*
+let userName = "Na   me";
+
+let result = userName.includes(" ");
+
+if (result) {
+    console.log("Your username cant include");
+}
+else {
+    console.log(userName);
+}
+*/
+
+//                               replaceAll methods
+//let phoneNumber = "123-456-7890";
+
+//phoneNumber = phoneNumber.replaceAll("-", "/");
+
+//console.log(phoneNumber);
+
+//                               padStart methods
+
+//let phoneNumber = "123-456-7890";
+
+//phoneNumber = phoneNumber.padStart(15, "0");
+
+//console.log(phoneNumber);
+
+//                               padEnd methods
+
+//let phoneNumber = "123-456-7890";
+
+//phoneNumber = phoneNumber.padEnd(15, "0");
+
+//console.log(phoneNumber);
